02ebe71c8da6c2b77d61034839efc5fa32ad986b
dostonbek <dostonbek@gmail.com>
2025-03-05 19:00:52 +0500

    Add fetching and displaying of product cards

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { fetchData } from "./fetchData.js";
+import { showCards } from "./updateUI.js";
+
+fetchData("https://dummyjson.com/product")
+  .then((data) => {
+    showCards(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
